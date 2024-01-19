8a0fa17b4396b8cea1c6a3b44d18a40d1df49c40
juh85 <julianamoraiscordeiro@hotmail.com>
2024-01-18 22:55:32 -0300

    incluindo sql

--- teste.php
+++ teste.php
@@ -7,3 +7,9 @@ $telefone = $_POST['tTelefone'];
 $mail = $_POST['tMail'];
 echo $nome . $idade . $estado . $telefone . $mail . '<br/>'; 
 echo '<a href="Pagina.html">Voltar</a>';
+
+
+
+$sqlInsert = "insert into cadastro (nome, idade, estado, telefone, email)
+values ('$nome', '$idade', '$estado', '$telefone', '$mail')";
+echo $sqlInsert;
