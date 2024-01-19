b865fd70ab0c4a8e34253cc64bd2bce920194828
juh85 <julianamoraiscordeiro@hotmail.com>
2024-01-18 21:18:45 -0300

    PHP
    Criando pagina de php

---
+++ teste.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <title>PHPPAGINA</title>
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <div>  
+         <?php 
+
+?>  
+
+
+    
+    <div>
+
+    </div>
+</body>
+</html>
